5f56aec2747a0eede1b11c1986f2636adf99328f
zhyrao <rain330047@outlook.com>
2018-04-27 14:44:59 +0800

    write unity outline shader

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    private GameObject obj;
+	// Use this for initialization
+	void Start () {
+        obj = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        obj.transform.Rotate(Vector3.up, 1, Space.Self);
+	}
+}
